02daca121ff5cb8782f296a573129f6b85eb817f
SinclaM <matthewsinclair2024@u.northwestern.edu>
2022-08-25 12:54:04 +0100

    Visualize with gnuplot

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,30 @@ pub mod physics;
 
 use crate::physics::shooting::ShootingSolverEven;
 use f64;
-use std::fs;
-use std::io::prelude::*;
+use std::fs::File;
+use std::io::Write;
 
 fn main() {
-    const N: usize = 100;
+    const N: usize = 10000;
     const DX: f64 = 1.0 / (N as f64);
-    const INITIAL_ENERGY: f64 = 0.0;
+    const INITIAL_ENERGY: f64 = 2.0;
     const DE: f64 = 10.0;
     const PSI_CUTOFF: f64 = 300.0;
     const ENERGY_STEP_CUTOFF: f64 = 0.0001;
 
     let mut solver = ShootingSolverEven::new(N, DX, INITIAL_ENERGY, DE, PSI_CUTOFF, box_potential, ENERGY_STEP_CUTOFF);
     solver.solve();
-    println!("{:?}", solver.wavefunction);
-    println!("{}", solver.energy);
 
-    let mut file = fs::OpenOptions::new().append(true).create(true).open("data.txt").expect("file open failure");
+    let mut file = File::create("data/square_well_shooting_method.txt").unwrap();
+    
+    let mut x: f64 = - (N as f64) * DX;
+    for ele in solver.wavefunction.iter().rev() {
+        write!(file, "{} {}\n", x, ele).unwrap();
+        x += DX;
+    }
     for ele in solver.wavefunction {
-        file.write_all((ele.to_string() + "\n").as_bytes()).expect("file write failure");
+        write!(file, "{} {}\n", x, ele).unwrap();
+        x += DX;
     }
 
 }
@@ -29,6 +34,6 @@ fn box_potential(x: f64) -> f64 {
     if x.abs() < 1.0 {
         0.0
     } else {
-        1000.0
+        100000.0
     }
 }
