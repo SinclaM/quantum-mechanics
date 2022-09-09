3479eaf67ccdf0386f37679b3b9c26de08164dfc
SinclaM <matthewsinclair2024@u.northwestern.edu>
2022-09-09 16:25:46 +0100

    Add node counting helper

--- src/utils.rs
+++ src/utils.rs
@@ -21,3 +21,25 @@ pub fn relative_error(observed: f64, theoretical: f64) -> f64 {
 
     relative_error
 }
+
+pub fn count_nodes(f: &[f64]) -> usize {
+    let mut nodes = 0;
+    for window in f.windows(2) {
+        if window[0].signum() != window[1].signum() {
+            nodes += 1;
+        }
+    }
+    nodes
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::{gen_range, count_nodes};
+    #[test]
+    fn test_count_nodes() {
+        let x_vals = gen_range(-2.0..=2.0, 0.01);
+        let y_vals: Vec<f64> = x_vals.iter().map(|x| x * x - 1.0).collect();
+        assert_eq!(count_nodes(&y_vals), 2);
+    }
+}
+
